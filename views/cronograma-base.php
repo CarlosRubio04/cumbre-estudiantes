9cc0269a06bacdcfb70c5648dc976927018e8a51
Carlos Rubio BM <crubio@bmdigital.co>
2018-05-09 20:27:41 -0500

    Cronograma para campamento Base, por meses

--- views/cronograma-base.php
+++ views/cronograma-base.php
@@ -5,7 +5,7 @@
 				<h1>Cronograma de actividades</h1>
 			</div>
 			
-			<!-- Aparece si no hay actividades -->
+			<!-- Aparece si no hay actividades 
 			<div class="noActivities">
 				<div class="message">
 					<p>Aún no tienes actividades en tu cronograma </p>
@@ -14,9 +14,122 @@
 					<a href="?content=agregar-actividad" class="btn-square">AGREGAR ACTIVIDAD</a>
 				</div>
 			</div>
-			<!-- ///Aparece si no hay actividades -->
+			///Aparece si no hay actividades -->
 
 			
+			<div class="cronograma-meses">
+				
+				<div class="cronograma-meses-container">
+					
+					<div class="month-container">
+						<div class="month-header">
+							Junio
+						</div>
+						
+						<div class="tasks-container">
+							<div class="task">
+								<a href="#" class="task-title">
+									Nombre de la tarea
+								</a>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae fugiat maxime obcaecati omnis voluptatum harum maiores numquam totam voluptates? Similique illo modi porro aliquam, excepturi fuga voluptatum at harum.
+								</p>
+								<div class="user-avatar">
+									<a href="#"	>
+										Jhon Doe
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+									</a>
+								</div>
+							</div>
+
+							<div class="task">
+								<a href="#" class="task-title">
+									Nombre de la tarea
+								</a>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae fugiat maxime obcaecati omnis voluptatum harum maiores numquam totam voluptates? Similique illo modi porro aliquam, excepturi fuga voluptatum at harum.
+								</p>
+								<div class="user-avatar">
+									<a href="#"	>
+										Jhon Doe
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+									</a>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="month-container">
+						<div class="month-header">
+							Julio
+						</div>
+						
+						<div class="tasks-container">
+							<div class="task">
+								<a href="#" class="task-title">
+									Nombre de la tarea
+								</a>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae fugiat maxime obcaecati omnis voluptatum harum maiores numquam totam voluptates? Similique illo modi porro aliquam, excepturi fuga voluptatum at harum.
+								</p>
+								<div class="user-avatar">
+									<a href="#"	>
+										Jhon Doe
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+									</a>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="month-container">
+						<div class="month-header">
+							Agosto
+						</div>
+						
+						<div class="tasks-container">
+							<div class="task">
+								<a href="#" class="task-title">
+									Nombre de la tarea
+								</a>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae fugiat maxime obcaecati omnis voluptatum harum maiores numquam totam voluptates? Similique illo modi porro aliquam, excepturi fuga voluptatum at harum.
+								</p>
+								<div class="user-avatar">
+									<a href="#"	>
+										Jhon Doe
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+									</a>
+								</div>
+							</div>
+
+							<div class="task">
+								<a href="#" class="task-title">
+									Nombre de la tarea
+								</a>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima recusandae fugiat maxime obcaecati omnis voluptatum harum maiores numquam totam voluptates? Similique illo modi porro aliquam, excepturi fuga voluptatum at harum.
+								</p>
+								<div class="user-avatar">
+									<a href="#"	>
+										Jhon Doe
+										<img src="http://placehold.it/30x30" alt="Avatar" class="img-circle">
+									</a>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+
 		</div>
 	</div>
 </section>
