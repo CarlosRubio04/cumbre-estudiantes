b874d423a42f4380570c8df773c186ce50145849
Carlos Rubio BM <crubio@bmdigital.co>
2018-05-09 20:36:01 -0500

    Nueva estructura html Perfil

--- views/perfil.php
+++ views/perfil.php
@@ -1,125 +1,148 @@
-<section class="main inner alistamiento">
-	<div class="alistamientoContainer">
-		<div class="profile">
-			<h1 class="title">
-				Mi perfil
-			</h1>
-			<div class="avatar">
-				<div class="avatarItems">
-					<button>
-						<img src="http://placehold.it/60x60" alt="Avatar">
-					</button>
+<section class="alistamiento">
+	<div class="container-flex">
+		<div class="profile item colum-40 u-color-contraste">
+			<div class="content-wrapper">
+				<h1 class="title">
+					Mi perfil
+				</h1>
+				<div class="avatar">
+					<h2 class="subTitle">
+						Selecciona tu avatar
+					</h2>
+					<div class="avatarItems">
+						<button>
+							<img src="http://placehold.it/40x40" alt="Avatar">
+						</button>
+						<button>
+							<img src="http://placehold.it/40x40" alt="Avatar">
+						</button>
+						<button>
+							<img src="http://placehold.it/40x40" alt="Avatar">
+						</button>
+						<button>
+							<img src="http://placehold.it/40x40" alt="Avatar">
+						</button>
+						<button>
+							<img src="http://placehold.it/40x40" alt="Avatar">
+						</button>
+					</div>
 				</div>
-			</div>
-			<div class="data">
-				<h4 class="headerLabel">
-					Verifica tu información
-				</h4>
-				<div class="dataGroup">
-					<small>Nombre</small>
-					<p>John</p>
-				</div>
-				<div class="dataGroup">
-					<small>Apellido</small>
-					<p>Doe</p>
-				</div>
-				<div class="dataGroup">
-					<small>E-mail</small>
-					<p>John-doe@gmail.com</p>
-				</div>
-			</div>
 
-			<div class="report">
-				<div class="dataGroup">
-					<small>Diagnostico personal</small>
-					<div class="pdf">
-						<div class="pdfIcon">
-							<a href="#">
-								<img src="img/pdf.svg" alt="PDF">
-							</a>
+				<!-- Datos del usuario -->
+				<div class="data-user">
+					<div class="data-group">
+						<div class="label">
+							<small>Nombre</small>
 						</div>
-						<div class="pdfInfo">
-							<a href="#">Ver Diagnostico</a>
-							<span>0099008800.pdf</span>
+						<div class="input">
+							<p>John</p>
+						</div>
+					</div>
+
+					<div class="data-group">
+						<div class="label">
+							<small>Apellido</small>
+						</div>
+						<div class="input">
+							<p>Doe</p>
+						</div>
+					</div>
+
+					<div class="data-group">
+						<div class="label">
+							<small>E-mail</small>
+						</div>
+						<div class="input">
+							<p>John-doe@gmail.com</p>
 						</div>
 					</div>
 				</div>
-			</div>
 
-
-			<div class="estadisticas">
-				<h3>Mis estadisticas de rendimiento</h3>
+				<div class="report">
+					<div class="dataGroup">
+						<small>Diagnostico personal</small>
+						<div class="pdf">
+							<div class="pdfIcon">
+								<a href="#">
+									<img src="img/pdf.svg" alt="PDF">
+								</a>
+							</div>
+							<div class="pdfInfo">
+								<a href="#">Ver Diagnostico</a>
+								<span>0099008800.pdf</span>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 
-		<div class="teamProfile">
-			<h4 class="title">
-				Brand Media Team
-			</h4>
+		<div class="teamProfile item colum-60">
+			<div class="content-wrapper">
+				<h4 class="subTitle">
+					Brand Media Team
+				</h4>
 
-			<div class="teamList">
-				<small>Integrantes del equipo</small>
+				<div class="teamList">
+					<small>Integrantes del equipo</small>
 
-				<div class="person">
-					<div class="avatar">
-						<img src="http://placehold.it/50x50" alt="Avatar">
-					</div>
-					<div class="info">
-						<p class="name">John Doe</p>
-						<p class="mail">jhon-doe@javeriana.com</p>
-					</div>
-				</div>
-
-				<div class="person">
-					<div class="avatar">
-						<img src="http://placehold.it/50x50" alt="Avatar">
-					</div>
-					<div class="info">
-						<p class="name">John Doe</p>
-						<p class="mail">jhon-doe@javeriana.com</p>
-					</div>
-				</div>
-
-				<div class="person">
-					<div class="avatar">
-						<img src="http://placehold.it/50x50" alt="Avatar">
-					</div>
-					<div class="info">
-						<p class="name">John Doe</p>
-						<p class="mail">jhon-doe@javeriana.com</p>
-					</div>
-				</div>
-
-				<div class="person">
-					<div class="avatar">
-						<img src="http://placehold.it/50x50" alt="Avatar">
-					</div>
-					<div class="info">
-						<p class="name">John Doe</p>
-						<p class="mail">jhon-doe@javeriana.com</p>
-					</div>
-				</div>
-			</div>
-
-			<div class="report">
-				<div class="dataGroup">
-					<small>Diagnostico grupal </small>
-					<div class="pdf">
-						<div class="pdfIcon">
-							<a href="#">
-								<img src="img/pdf.svg" alt="PDF">
-							</a>
+					<div class="person">
+						<div class="avatar">
+							<img src="http://placehold.it/50x50" alt="Avatar">
 						</div>
-						<div class="pdfInfo">
-							<a href="#">Ver Diagnostico</a>
-							<span>0099008800.pdf</span>
+						<div class="info">
+							<p class="name">John Doe</p>
+							<p class="mail">jhon-doe@javeriana.com</p>
+						</div>
+					</div>
+
+					<div class="person">
+						<div class="avatar">
+							<img src="http://placehold.it/50x50" alt="Avatar">
+						</div>
+						<div class="info">
+							<p class="name">John Doe</p>
+							<p class="mail">jhon-doe@javeriana.com</p>
+						</div>
+					</div>
+
+					<div class="person">
+						<div class="avatar">
+							<img src="http://placehold.it/50x50" alt="Avatar">
+						</div>
+						<div class="info">
+							<p class="name">John Doe</p>
+							<p class="mail">jhon-doe@javeriana.com</p>
+						</div>
+					</div>
+
+					<div class="person">
+						<div class="avatar">
+							<img src="http://placehold.it/50x50" alt="Avatar">
+						</div>
+						<div class="info">
+							<p class="name">John Doe</p>
+							<p class="mail">jhon-doe@javeriana.com</p>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div class="estadisticas">
-				<h3>Estadisticas de rendimiento grupales</h3>
+				<div class="report">
+					<div class="dataGroup">
+						<small>Diagnostico personal</small>
+						<div class="pdf">
+							<div class="pdfInfo">
+								<a href="#">Ver Diagnostico</a>
+								<span>0099008800.pdf</span>
+							</div>
+							<div class="pdfIcon">
+								<a href="#">
+									<img src="img/pdf.svg" alt="PDF">
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
